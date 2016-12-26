a2f436aa10eb259ff00d3e6d6dc469b143a62543
Tony <1431799680@qq.com>
2016-12-26 15:47:49 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -20,6 +20,7 @@ import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
+
 import org.litepal.crud.DataSupport;
 
 import java.io.IOException;
@@ -56,7 +57,7 @@ public class ChooseAreaFragment extends Fragment {
     /**
      * 县列表
      */
-    private List<County>countyList;
+    private List<County> countyList;
     /**
      * 选中的省
      */
@@ -93,12 +94,19 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
-                }else if(currentLevel==LEVEL_COUNTY){
-                    String weatherId=countyList.get(position).getWeatherId();
-                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
@@ -157,8 +165,6 @@ public class ChooseAreaFragment extends Fragment {
             queryFromServer(address, "city");
         }
     }
-
-
 
 
     /**
